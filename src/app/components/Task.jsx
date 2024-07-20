e93e08937b79bbd51b555a46c4bf7926d8813d74
ppp <Natdanai_kamthorn692547?>
2024-07-20 17:31:08 +0700

    Natdanai

--- src/app/components/Task.jsx
+++ src/app/components/Task.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 export default function Task({ task }) {
     return (
       <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-        {task.isDone ? (<span className="text-decoration-line-through">{task.title}</span> ) : (
-          <span>{task.title}</span>)}
-          
+        {task.isDone ? (<span className="text-decoration-line-through">{task.title}</span> ) : (<span>{task.title}</span>)}
+
         <button className="btn btn-success">Done</button>
         <button className="btn btn-danger">Delete</button>
       </div>
